b61d69a09682ca2ecad5eb211b585f45d869592f
lype98 <ultimatelype98@gmail.com>
2021-01-14 11:23:11 +0000

    create the template subpage

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ A Dark Souls III message simulator
 
 TODO:
 <!-- 1. change the main page to localhost/ds3 so I can start adding submitted templates to the database -->
-send AJAX POST request to server
-2. a button to submit a template, creating a path number and sending it to the DB
-2a. make sure there's no duplicate path or message
+<!-- 2. a button to submit a template, creating a path number and sending it to the DB
+2a. make sure there's no duplicate path or message -->
 3. a way to enter a path in the URL and it loading a page with the message
+3a. create a .ejs view that will be pretty much a copy of ds3.ejs but it shows just the template, being able to appraise and disparge
+3b. in ds3.js, do something like a router.get('/:path',...) that will get that path from the database and fill the template in
--- routes/ds3.js
+++ routes/ds3.js
@@ -59,16 +59,59 @@ router.get('/', (req, res) => {
     }; queryWrite()
 });
 
+router.get('/:path', async(req, res) => { // page that will load just the template from the DB
+    let soapstone = {
+        message: '',
+        appraisals: ''
+    }
+    await db.query('SELECT message,path,appraisals FROM ds3messages.soapstones', (error,results,fields) => {        
+        let current = 0;
+        for(result of results) {
+            current++
+            if(result.path === parseInt(req.params.path)) { // if path exists in DB, fill soapstone object and render template page
+                soapstone.message = result.message;
+                soapstone.appraisals = result.appraisals;
+                console.log(soapstone);
+                return res.render('ds3-template', {soapstone: soapstone})
+            }
+            if(current >= results.length) {
+                res.status(404).send("Error 404 - This message path doesn't exist") // if loop ends without finding anything, send 404
+            }            
+        }
+    });
+});
+
+
+
 router.post('/test', async(req, res)=> {
     const soapstone = req.body;
     // query db, check if there is already another message that's the same. if there is, ask user if they want to visit the link, else, check path
     await db.query('SELECT message,path FROM ds3messages.soapstones', (error,results,fields) => {
         if (error) throw error;
+        const sendToDB = ()=> { // function to send the soapstone to the DB            
+            db.query(`INSERT INTO ds3messages.soapstones (message,path) VALUES ("${soapstone.message}","${soapstone.path}");`, (error,results,fields) => {
+                if (error) throw error;
+            });
+            // console.log(`INSERT INTO ds3messages.soapstones (message,path) VALUES ('${soapstone.message}','${soapstone.path}');`);
+        };
         for (result of results) {            
             if(result.message == soapstone.message) { /* IMPORTANT, change this console log to a redirect prompt in the front end to /ds3/result.path */
                 console.log(`this message already exists! path: ${result.path}`);
             }
-          }
+            if(result.path == soapstone.path){ // if soapstone path exists in DB, create 10 paths, try them until it sends the updated soapstone
+                const array = new Array(10).fill(0);
+                const potentialPaths = array.map(path => path + Math.floor(Math.random() * 99999));
+                for (potentialPath of potentialPaths) {
+                    if(potentialPath !== result.path){                        
+                        soapstone.path = potentialPath;
+                        return sendToDB();
+                    };
+                };
+            }            
+            else{
+                return sendToDB(); // IMPORTANT BUG: CLICKING SUBMIT AFTER THE 1ST CLICK SENDS DUPLICATE TO DB. I NEED TO REDIRECT TO THE NEW TEMPLATE WEBSITE
+            }
+          };
     });
     //query db, check path, if there's already another one, reroll a few more random ones and check again
     //if all successfull, redirect to new page with the submitted message
